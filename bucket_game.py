607df1fa6952de6ef85d152ace58fbecda3c6e16
Vegard Aa Albretsen <vegardaaalbretsen@gmail.com>
2025-10-14 17:03:15 +0200

    Minmax added to bucket game

--- bucket_game.py
+++ bucket_game.py
@@ -36,9 +36,48 @@ class Game:
         assert type(actions[0]) is int
         return actions[0] if player == self.to_move(state) else -actions[0]
 
-    def print(self, state):
+    def print(self, state: State):
         print(f'The state is {state} and ', end='')
         if self.is_terminal(state):
             print(f'P1\'s utility is {self.utility(state, 0)}')
         else:
             print(f'it is P{self.to_move(state)+1}\'s turn')
+
+def minimax_search(game: Game, state: State) -> Action | None:
+    player = game.to_move(state)
+    _, move = max_value(game, state, player)
+    return move
+def max_value(game: Game, state: State, player: int) -> tuple[float, Action | None]:
+    if game.is_terminal(state):
+        return game.utility(state, player), None
+    v = float("-inf")
+    best: Action | None = None
+    for a in game.actions(state):
+        v2, _ = min_value(game, game.result(state, a), player)
+        if v2 > v:
+            v, best = v2, a
+    return v, best
+
+def min_value(game:Game, state: State, player: int) -> tuple[float, Action | None]:
+    if game.is_terminal(state):
+        return game.utility(state,player), None
+    v = float("-inf")
+    best: Action | None = None
+    for a in game.actions(state):
+        v2, _ = max_value(game, game.result(state,a), player)
+        if v2 > v: 
+            v, best = v2, a
+    return v, best
+
+game = Game()
+
+state = game.initial_state()
+game.print(state)
+while not game.is_terminal(state):
+    player = game.to_move(state)
+    action = minimax_search(game, state) # The player whose turn it is
+                                         # is the MAX player
+    print(f'P{player+1}\'s action: {action}')
+    assert action is not None
+    state = game.result(state, action)
+    game.print(state)
